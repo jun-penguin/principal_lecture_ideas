284f63de3b860407c46b13ae0309465e2a0e0600
Jun Oda <b2410723@gmail.com>
2022-04-23 00:08:40 +0900

    #11 postモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_21_063501) do
+ActiveRecord::Schema.define(version: 2022_04_22_084825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.text "content", null: false
+    t.integer "status", default: 0, null: false
+    t.integer "grade_range", default: 0, null: false
+    t.integer "scene_type", default: 0, null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -29,4 +42,5 @@ ActiveRecord::Schema.define(version: 2022_04_21_063501) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
