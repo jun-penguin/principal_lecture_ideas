c2df12022767934392c73c12f9fb08db0c4d7a9f
Jun Oda <b2410723@gmail.com>
2022-05-27 14:48:07 +0900

    Change: 検索条件に合致する講話がない場合の表示を設定、ログインユーザー名の表示条件を変更 #56

---
+++ app/javascript/components/TopPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid fill-height>
+    <v-row>
+      <v-col>
+        <v-img class="image"  
+          lazy-src="https://picsum.photos/id/11/10/6"
+          
+          src="https://picsum.photos/id/11/500/300"
+        ></v-img>
+        <p>校長講話のアイディアを集積する.</p>
+        <p>子どもたちの将来のため.</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+  .image {
+    width: 100%;
+  }
+</style>
