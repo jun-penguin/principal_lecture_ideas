6b8ce590cd16edb6b78bc7f3dead30783b872d09
Jun Oda <b2410723@gmail.com>
2022-08-01 15:39:50 +0900

    Add: postモデルのモデルスペックを実装 #85

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,5 +16,71 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '必須カラムのvalidationの検証' do
+    context '全必須カラムが指定されているとき' do
+      let(:post) { build(:post) }
+      it '正常にpostが登録される' do
+        expect(post).to be_valid
+      end
+    end
+
+    context 'タイトルが指定されていないとき' do
+      let(:post) { build(:post, title: nil) }
+      it 'エラーになる' do
+        post.valid?
+        expect(post.errors[:title]).to include('が入力されていません。')
+      end
+    end
+  end
+
+  context '講話紹介が入力されていないとき' do
+    let(:post) { build(:post, description: nil) }
+    it 'エラーになる' do
+      post.valid?
+      expect(post.errors[:description]).to include('が入力されていません。')
+    end
+  end
+
+  context '講話紹介が入力されていないとき' do
+    let(:post) { build(:post, description: nil) }
+    it 'エラーになる' do
+      post.valid?
+      expect(post.errors[:description]).to include('が入力されていません。')
+    end
+  end
+  context '本文が入力されていないとき' do
+    let(:post) { build(:post, content: nil) }
+    it 'エラーになる' do
+      post.valid?
+      expect(post.errors[:content]).to include('が入力されていません。')
+    end
+  end
+  context 'ステータスが指定されていないとき' do
+    let(:post) { build(:post, status: nil) }
+    it 'エラーになる' do
+      post.valid?
+      expect(post.errors[:status]).to include('が入力されていません。')
+    end
+  end
+  context '対象が指定されていないとき' do
+    let(:post) { build(:post, grade_range: nil) }
+    it 'エラーになる' do
+      post.valid?
+      expect(post.errors[:grade_range]).to include('が入力されていません。')
+    end
+  end
+  context '対象が指定されていないとき' do
+    let(:post) { build(:post, grade_range: nil) }
+    it 'エラーになる' do
+      post.valid?
+      expect(post.errors[:grade_range]).to include('が入力されていません。')
+    end
+  end
+  context '対象学年が指定されていないとき' do
+    let(:post) { build(:post, scene_type: nil) }
+    it 'エラーになる' do
+      post.valid?
+      expect(post.errors[:scene_type]).to include('が入力されていません。')
+    end
+  end
 end
