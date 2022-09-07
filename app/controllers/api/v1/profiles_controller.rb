eaebe404547384bc06e21483797f134264df324f
Jun Oda <b2410723@gmail.com>
2022-09-08 04:06:40 +0900

    Change: 他ユーザーのプロフィール画面の投稿一覧をupdate順になるように変更 #112

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::ProfilesController < Api::V1::ApplicationController
 
   def show
     @user = User.find_by(name: params[:username])
-    @posts = @user.posts.published
+    @posts = @user.posts.published.order(updated_at: :desc)
     render :show, formats: :json, handlers: 'jbuilder'
   end
 
